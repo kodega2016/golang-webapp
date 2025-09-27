81bab5cc53c9a62fc221510c3e5be2c72f5e3bcd
kodega2016 <khadgalovecoding2016@gmail.com>
2025-09-28 03:24:48 +0545

    feat:updated routes with chi router

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,13 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"github.com/kodega2016/booking-app/pkg/config"
 	"github.com/kodega2016/booking-app/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
