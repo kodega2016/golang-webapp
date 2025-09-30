6258c76f41f318b6a9d84b554bf82690572ff4ca
kodega2016 <khadgalovecoding2016@gmail.com>
2025-09-30 08:44:55 +0545

    feat:added form validation for the empty

--- internal/pkg/forms/forms.go
+++ internal/pkg/forms/forms.go
@@ -1,8 +1,10 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Form is custom data types with url values and errors
@@ -19,7 +21,35 @@ func New(data url.Values) *Form {
 	}
 }
 
+// Has check the form has the specific field value or not
 func (form *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
-	return x != ""
+	if x == "" {
+		return false
+	}
+	return true
+}
+
+// Valid returns the validation state of the form
+func (form *Form) Valid() bool {
+	return len(form.Errors) == 0
+}
+
+// Required will check empty value valition for multiple fields
+func (form *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := form.Get(field)
+		if strings.TrimSpace(value) == "" {
+			form.Errors.Add(field, fmt.Sprintf("The %s cannot be blank.\n", field))
+		}
+	}
+}
+
+func (form *Form) MinLength(field string, length int, r *http.Request) bool {
+	value := r.Form.Get(field)
+	if len(value) < length {
+		form.Errors.Add(field, fmt.Sprintf("This %s must be at least %d characters long.", field, length))
+		return false
+	}
+	return true
 }
--- internal/pkg/handlers/handlers.go
+++ internal/pkg/handlers/handlers.go
@@ -50,13 +50,44 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation handles the rendering of the reservation form template
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
 // PostReservation handles the submition of the forms
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Phone:     r.Form.Get("phone"),
+		Email:     r.Form.Get("email"),
+	}
+
+	form := forms.New(r.PostForm)
+	form.Required("first_name", "last_name", "email", "phone")
+	form.MinLength("first_name", 4, r)
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+	}
 }
 
 // Generals handles the rendering of the generals page
